9fb7cdb29da3990ffe329bb2a3d09d91ac7caa6d
kzerbe <klaus@zerbe-net.org>
2018-12-08 08:29:23 +0100

    initial checkin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SvgFaderComponent} from './components/svg-fader/svg-fader.component';
+import {CssFaderComponent} from './components/css-fader/css-fader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'css-test', pathMatch: 'full'},
+  {path: 'svg-test', component: SvgFaderComponent },
+  {path: 'css-test', component: CssFaderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SvgFaderComponent} from './components/svg-fader/svg-fader.component';
+import {CssFaderComponent} from './components/css-fader/css-fader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CssFaderComponent,
+    SvgFaderComponent
   ],
   imports: [
     BrowserModule,
