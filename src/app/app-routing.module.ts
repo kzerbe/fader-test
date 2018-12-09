eee63f29b7ad5a0b77a905b3ebd5015d5446c556
kzerbe <klaus@zerbe-net.org>
2018-12-09 22:05:43 +0100

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {useHash: true, enableTracing: false}),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
